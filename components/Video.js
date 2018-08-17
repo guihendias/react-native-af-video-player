d73ed8f16c769378ecc9e5ddf7179d7bf4758a0d
Guilherme <guilhermehenriquedias@hotmail.com>
2018-08-17 17:00:10 -0300

    Adicionando height via props

--- components/Video.js
+++ components/Video.js
@@ -66,7 +66,7 @@ class Video extends Component {
       seeking: false,
       renderError: false
     }
-    this.animInline = new Animated.Value(Win.width * 0.5625)
+    this.animInline = new Animated.Value(props.playerHeight)
     this.animFullscreen = new Animated.Value(Win.width * 0.5625)
     this.BackHandler = this.BackHandler.bind(this)
     this.onRotated = this.onRotated.bind(this)
